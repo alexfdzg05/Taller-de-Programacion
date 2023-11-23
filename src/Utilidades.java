d82b3b2d982b1f89b55efb1af5b0fd54b06f8857
Aconi-dot <90859549+Aconi-dot@users.noreply.github.com>
2023-11-24 00:15:27 +0100

    LeerNum completado en Utilidades

--- src/Utilidades.java
+++ src/Utilidades.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 /**
  * Description of the class
  *
- * @author
+ * @author Paula Cabrero
  * @author
  * @version     1.0
  */
@@ -15,14 +15,16 @@ public class Utilidades {
      * TODO: Solicita un número repetidamente hasta que se introduzca uno correcto (dentro de los límites)
      * @param teclado
      * @param mensaje
-     * @param minimo
-     * @param maximo
+     * @param minimo el valor mínimo que se tiene que encontrar el numero
+     * @param maximo el valor máximo que se tiene que encontrar el numero
      * @return int numero
      */
     public static int leerNumero(Scanner teclado, String mensaje, int minimo, int maximo) {
         int numero;
-
-
+        do{
+            System.out.println(mensaje);
+            numero=teclado.nextInt();
+        }while (!(numero >= minimo && numero <= maximo));
         return numero;
     }
 
@@ -30,14 +32,19 @@ public class Utilidades {
      * TODO: Solicita un número repetidamente hasta que se introduzca uno correcto (dentro de los límites)
      * @param teclado
      * @param mensaje
-     * @param minimo
-     * @param maximo
+     * @param minimo  el valor mínimo que se tiene que encontrar el numero
+     * @param maximo el valor máximo que se tiene que encontrar el numero
      * @return long numero
      */
     public static long leerNumero(Scanner teclado, String mensaje, long minimo, long maximo) {
-        long numero;
-
-
+        System.out.println(mensaje);
+        long numero = teclado.nextLong();
+        teclado.nextLine();
+        while (!(numero >= minimo && numero <= maximo)) {
+            System.out.println(mensaje);
+            numero = teclado.nextLong();
+            teclado.nextLine();
+        }
         return numero;
     }
 
@@ -45,78 +52,22 @@ public class Utilidades {
      * TODO: Solicita un número repetidamente hasta que se introduzca uno correcto (dentro de los límites)
      * @param teclado
      * @param mensaje
-     * @param minimo
-     * @param maximo
+     * @param minimo el valor mínimo que se tiene que encontrar el numero
+     * @param maximo el valor máximo que se tiene que encontrar el numero
      * @return double numero
      */
     public static double leerNumero(Scanner teclado, String mensaje, double minimo, double maximo) {
-        double numero;
-
+        System.out.println(mensaje);
+        double numero = teclado.nextDouble();
+        teclado.nextLine();
+        while (!(numero >= minimo && numero <= maximo)) {
+            System.out.println(mensaje);
+            numero = teclado.nextDouble();
+            teclado.nextLine();
+        }
         return numero;
     }
 
-    /**
-     * TODO: TODO: Solicita una letra repetidamente hasta que se introduzca uno correcto (dentro de los límites)
-     * @param teclado
-     * @param mensaje
-     * @param minimo
-     * @param maximo
-     * @return char letra
-     */
-    public static char leerLetra(Scanner teclado, String mensaje, char minimo, char maximo) {
-        char letra;
 
-
-
-        return letra;
-    }
-
-
-    /**
-     * TODO: Solicita una fecha repetidamente hasta que se introduzca una correcta
-     * @param teclado
-     * @param mensaje
-     * @return Fecha
-     */
-    public static Fecha leerFecha(Scanner teclado, String mensaje) {
-        int dia;
-        int mes;
-        int anio;
-
-
-
-        return new Fecha(dia, mes, anio);
-    }
-
-
-    /**
-     * TODO: Solicita una fecha y hora repetidamente hasta que se introduzcan unas correctas
-     * @param teclado
-     * @param mensaje
-     * @return Fecha
-     */
-    public static Fecha leerFechaHora(Scanner teclado, String mensaje) {
-        int dia;
-        int mes;
-        int anio;
-        int hora;
-        int minuto;
-        int segundo;
-
-
-
-        return new Fecha(dia, mes, anio, hora, minuto, segundo);
-    }
-
-    /**
-     * TODO: Imprime por pantalla el String pasado por parámetro
-     * @param teclado
-     * @param s
-     * @return
-     */
-    public static String leerCadena(Scanner teclado, String s) {
-        System.out.print(s);
-        return teclado.next();
-    }
 }
 
