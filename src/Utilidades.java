a58c3689829d8467497259abb83b1f16308d6a2c
Aconi-dot <90859549+Aconi-dot@users.noreply.github.com>
2023-11-24 00:39:10 +0100

    LeerFecha y leerFechaHora completados en Utilidades

--- src/Utilidades.java
+++ src/Utilidades.java
@@ -86,5 +86,83 @@ public class Utilidades {
     }
 
 
+    /**
+     * TODO: Solicita una fecha repetidamente hasta que se introduzca una correcta
+     * @param teclado
+     * @param mensaje
+     * @return Fecha
+     */
+    public static Fecha leerFecha(Scanner teclado, String mensaje) {
+        int dia, mes, anio;
+        boolean correcto = false;
+        Fecha fecha = null;
+        System.out.println(mensaje);
+        do {
+            String mensajeDias = "Ingrese día: ";
+            dia = leerNumero(teclado, mensajeDias, 1, Fecha.DIAS_MES);
+            String mensajeMes = "Ingrese mes: ";
+            mes = leerNumero(teclado, mensajeMes, 1, Fecha.MESES_ANIO);
+            String mensajeAnio = "Ingrese año: ";
+            anio = leerNumero(teclado, mensajeAnio, Fecha.PRIMER_ANIO, Fecha.ULTIMO_ANIO);
+            if(Fecha.comprobarFecha(dia,mes,anio)){
+                correcto=true;
+            }else {
+                System.out.println("Fecha introducida incorrecta.");
+            }
+        } while (!correcto);
+        if (!Fecha.comprobarFecha(fecha.getDia(), fecha.getMes(), fecha.getAnio())) {
+            // La fecha es incorrecta, se lanza una excepción o se devuelve null
+            throw new IllegalArgumentException("Fecha introducida incorrecta.");
+            // O bien: fecha = null;
+        }
+        return new Fecha(dia, mes, anio);
+    }
+
+
+    /**
+     * TODO: Solicita una fecha y hora repetidamente hasta que se introduzcan unas correctas
+     * @param teclado
+     * @param mensaje
+     * @return Fecha
+     */
+    public static Fecha leerFechaHora(Scanner teclado, String mensaje) {
+        int dia, mes, anio, hora, minuto, segundo;
+        Fecha fecha ;
+        boolean correcto = false;
+        System.out.println(mensaje);
+        do {
+            String mensajeDias = "Ingrese día: ";
+            dia = leerNumero(teclado, mensajeDias, 1, Fecha.DIAS_MES);
+            String mensajeMes = "Ingrese mes: ";
+            mes = leerNumero(teclado, mensajeMes, 1, Fecha.MESES_ANIO);
+            String mensajeAnio = "Ingrese año: ";
+            anio = leerNumero(teclado, mensajeAnio, Fecha.PRIMER_ANIO, Fecha.ULTIMO_ANIO);
+            String mensajeHora = "Ingrese hora: ";
+            hora = leerNumero(teclado, mensajeHora, 0, Fecha.HORAS_DIA);
+            String mensajeMinuto = "Ingrese minuto: ";
+            minuto = leerNumero(teclado, mensajeMinuto, 0, Fecha.MINUTOS_HORA);
+            String mensajeSegundo = "Ingrese segundo: ";
+            segundo = leerNumero(teclado, mensajeSegundo, 0, Fecha.SEGUNDOS_MINUTO);
+
+            if(Fecha.comprobarFecha(dia,mes,anio) && Fecha.comprobarHora(hora,minuto,segundo)){
+                correcto=true;
+            }else {
+                System.out.println("Fecha u hora introducida incorrecta.");
+            }
+
+        } while (!correcto);
+        return new Fecha(dia, mes, anio, hora, minuto, segundo);
+    }
+
+    /**
+     * TODO: Imprime por pantalla el String pasado por parámetro
+     * @param teclado
+     * @param s
+     * @return
+     */
+    public static String leerCadena(Scanner teclado, String s) {
+        System.out.print(s);
+        return teclado.next();
+    }
 }
 
