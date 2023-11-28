51c6c07ea25d9d57aeef01c8deacc05856fb933e
Aconi-dot <90859549+Aconi-dot@users.noreply.github.com>
2023-11-28 15:18:19 +0100

    Métodos cortos de la clase Cliente creados

--- src/Cliente.java
+++ src/Cliente.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
  * Description of the class
  *
  * @author Taller de Programación
+ * @author Paula Cabrero Carrillo
+ * @author Alejandro Fernández Guerrero
  * @version     1.0
  */
 public class Cliente {
@@ -39,29 +41,34 @@ public class Cliente {
     }
     // TODO: Texto que debe generar: Zapp Brannigan, zapp.brannigan@dop.gov
     public String toString() {
-
+        return nombre + " " + apellidos + ", " + email;
     }
     // TODO: Devuelve un booleano que indica si se ha alcanzado el número máximo de envíos
     public boolean maxEnviosAlcanzado() {
-
+        return listaEnvios.estaLlena();
     }
     // TODO: Devuelve un envío en función de su posición
     public Envio getEnvio(int i) {
-
+        return listaEnvios.getEnvio(i);
     }
     public ListaEnvios getListaEnvios() {
         return listaEnvios;
     }
     // TODO: Añade un envío al cliente
     public boolean aniadirEnvio(Envio envio) {
-
+        boolean aniadido = false;
+        if(!maxEnviosAlcanzado()){
+            listaEnvios.insertarEnvio(envio);
+            aniadido = true;
+        }
+        return aniadido;
     }
     public Envio buscarEnvio(String localizador) {
         return listaEnvios.buscarEnvio(localizador);
     }
     // TODO: Elimina el envío de la lista de envíos del pasajero
     public boolean cancelarEnvio(String localizador) {
-
+        return listaEnvios.eliminarEnvio(localizador);
     }
     public void listarEnvios() {
         listaEnvios.listarEnvios();
