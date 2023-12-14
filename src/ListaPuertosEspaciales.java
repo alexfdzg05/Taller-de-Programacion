c99d3962ab58c1893ca78c79b97c0b204e071a5b
Alejandro <alexfdzg05@gmail.com>
2023-12-14 19:01:54 +0100

    Solucionado un error en el insertarPuerto que hacía que no puediéramos leer el fichero (nuevo problema: En lista Envios no nos coge el precio con el parseInt)

--- src/ListaPuertosEspaciales.java
+++ src/ListaPuertosEspaciales.java
@@ -68,6 +68,7 @@ public class ListaPuertosEspaciales {
                 i++;
             }
             lista[i] = puertoEspacial;
+            esPosible = true;
         }
         return esPosible;
     }
