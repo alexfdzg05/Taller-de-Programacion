3d0fc342d16ee7b3c10c08f4d5eae160bc617c8a
Aconi-dot <90859549+Aconi-dot@users.noreply.github.com>
2023-12-22 11:58:01 +0100

    Clase ListaEnvios completamente comentada.

--- src/ListaEnvios.java
+++ src/ListaEnvios.java
@@ -3,7 +3,9 @@ import java.io.*;
 import java.util.Scanner;
 
 /**
- * Description of the class
+ * Clase que representa una lista de envíos. Contiene métodos para gestionar y manipular
+ * envíos, como la inserción, búsqueda, eliminación y visualización de los mismos.
+ * Además, permite realizar operaciones de lectura y escritura desde y hacia ficheros CSV.
  *
  * @author Paula Cabrero
  * @author Alejandro Fernández
@@ -16,13 +18,16 @@ public class ListaEnvios {
     /**
      * TODO: Constructor de la clase para inicializar la lista a una capacidad determinada
      *
-     * @param capacidad
+     * @param capacidad Capacidad inicial de la lista de envíos
      */
     public ListaEnvios(int capacidad) {
         envios = new Envio[capacidad];
     }
 
     // TODO: Devuelve el número de envíos que hay en la lista
+    /**
+     * @return número de envíos en la lista
+     */
     public int getOcupacion() {
         int ocupacion = 0;
         for (int i = 0; i < envios.length; i++) {
@@ -32,11 +37,20 @@ public class ListaEnvios {
         }
         return ocupacion;
     }
+
+    /**
+     * Devuelve la longitud máxima de la lista de envíos
+     *
+     * @return longitud máxima de la lista de envíos
+     */
     public int getLength(){
         return envios.length;
     }
 
     // TODO: ¿Está llena la lista de envíos?
+    /**
+     * @return true si la lista está llena, false si hay espacio disponible.
+     */
     public boolean estaLlena() {
         boolean llena = true;
         int i = 0;
@@ -50,6 +64,10 @@ public class ListaEnvios {
     }
 
     //TODO: Devuelve el envio dado un indice
+    /**
+     * @param i Índice del envío
+     * @return Envío en la posición indicada
+     */
     public Envio getEnvio(int i) {
         return envios[i];
     }
@@ -57,7 +75,7 @@ public class ListaEnvios {
     /**
      * TODO: insertamos un nuevo envío en la lista
      *
-     * @param envio
+     * @param envio Envío a ser insertado
      * @return true en caso de que se añada correctamente, false en caso de lista llena o error
      */
     public boolean insertarEnvio(Envio envio) {
@@ -76,7 +94,7 @@ public class ListaEnvios {
     /**
      * TODO: Buscamos el envio a partir del localizador pasado como parámetro
      *
-     * @param localizador
+     * @param localizador Localizador del envío a buscar
      * @return el envio que encontramos o null si no existe
      */
     public Envio buscarEnvio(String localizador) {
@@ -94,9 +112,9 @@ public class ListaEnvios {
     /**
      * TODO: Buscamos el envio a partir del idPorte, fila y columna pasados como parámetros
      *
-     * @param idPorte
-     * @param fila
-     * @param columna
+     * @param idPorte Identificador del porte asociado al envío
+     * @param fila    Fila del envío
+     * @param columna Columna del envío
      * @return el envio que encontramos o null si no existe
      */
     public Envio buscarEnvio(String idPorte, int fila, int columna) {
@@ -118,7 +136,7 @@ public class ListaEnvios {
     /**
      * TODO: Eliminamos un envio a través del localizador pasado por parámetro
      *
-     * @param localizador
+     * @param localizador Localizador del envío a eliminar
      * @return True si se ha borrado correctamente, false en cualquier otro caso
      */
     public boolean eliminarEnvio(String localizador) {
@@ -147,110 +165,112 @@ public class ListaEnvios {
         }
     }
 
-        /**
-         * TODO: Permite seleccionar un Envio existente a partir de su localizador, usando el mensaje pasado como argumento
-         *  para la solicitud y siguiendo el orden y los textos mostrados en el enunciado.
-         *  La función solicita repetidamente hasta que se introduzca un localizador correcto
-         * @param teclado
-         * @param mensaje
-         * @return
-         */
-        public Envio seleccionarEnvio (Scanner teclado, String mensaje){
-            Envio envio = null;
-            String localizador;
-            do {
-                localizador = Utilidades.leerCadena(teclado, mensaje);
-                if (!localizador.equalsIgnoreCase("cancelar")) {
-                    envio = buscarEnvio(localizador);
-                    if (envio == null) {
-                        System.out.println("Localizador incorrecto: ");
-                    }
-                }
-            }while (envio == null && !localizador.equalsIgnoreCase("cancelar"));
-            return envio;
-        }
-
-
-        /**
-         * TODO: Añade los Envios al final de un fichero CSV, SIN SOBREESCRIBIR la información
-         * @param fichero
-         * @return
-         */
-        public boolean aniadirEnviosCsv (String fichero){
-            PrintWriter pw = null;
-            try {
-                pw = new PrintWriter(fichero);
-                for (int i = 0; i < envios.length; i++){
-                    if (envios[i]!= null) {
-                        pw.print(envios[i].getLocalizador());
-                        pw.print(";");
-                        pw.print(envios[i].getPorte().getID());
-                        pw.print(";");
-                        pw.print(envios[i].getCliente().getEmail());
-                        pw.print(";");
-                        pw.print(envios[i].getFila());
-                        pw.print(";");
-                        pw.print(envios[i].getColumna());
-                        pw.print(";");
-                        pw.print(envios[i].getPrecio());
-                        pw.println();
-                    } else {
-                        pw.println();
-                    }
-                }
-                return true;
-            } catch (FileNotFoundException e) {
-                return false;
-            } finally {
-                if (pw != null) {
-                    pw.close();
+    /**
+     * TODO: Permite seleccionar un Envio existente a partir de su localizador, usando el mensaje pasado como argumento
+     *  para la solicitud y siguiendo el orden y los textos mostrados en el enunciado.
+     *  La función solicita repetidamente hasta que se introduzca un localizador correcto
+     *
+     * @param teclado Scanner para la entrada de texto
+     * @param mensaje Mensaje de solicitud para el localizador
+     * @return El envío seleccionado o null si se cancela la operación
+     */
+    public Envio seleccionarEnvio (Scanner teclado, String mensaje){
+        Envio envio = null;
+        String localizador;
+        do {
+            localizador = Utilidades.leerCadena(teclado, mensaje);
+            if (!localizador.equalsIgnoreCase("cancelar")) {
+                envio = buscarEnvio(localizador);
+                if (envio == null) {
+                    System.out.println("Localizador incorrecto: ");
                 }
             }
-        }
+        }while (envio == null && !localizador.equalsIgnoreCase("cancelar"));
+        return envio;
+    }
 
-        /**
-         * TODO: Lee los Envios del fichero CSV y los añade a las listas de sus respectivos Portes y Clientes
-         * @param ficheroEnvios
-         * @param portes
-         * @param clientes
-         */
-        public static void leerEnviosCsv (String ficheroEnvios, ListaPortes portes, ListaClientes clientes) {
-            ListaEnvios listaEnvios = new ListaEnvios(clientes.getLength());
-            Scanner sc = null;
-            boolean escrito = true;
-            try {
-                sc = new Scanner(new FileReader(ficheroEnvios));
-                String linea;
 
-                while (sc.hasNextLine() && escrito) {
-                    linea = sc.nextLine();
-                    String[] datos = linea.split(";");
-                    String localizador = datos[0];
-                    Porte porte = portes.buscarPorte(datos[1]);
-                    Cliente cliente = clientes.buscarClienteEmail(datos[2]);
-                    int fila = Integer.parseInt(datos[3]);
-                    int columna = Integer.parseInt(datos[4]);
-                    double precio = Double.parseDouble(datos[5]);
-//HERE Error de la lista
-                    Envio envio = new Envio(localizador, porte, cliente, fila, columna, precio);
-                    if (porte != null) {
-                        porte.ocuparHueco(envio);
-                    }
-
-                    if (cliente != null) {
-                        cliente.aniadirEnvio(envio);
-                    }
-                    escrito = listaEnvios.insertarEnvio(envio);//HERE revisar el ocuparHueco
-
-                }
-            } catch (FileNotFoundException e) {
-                System.out.println("No se ha encontrado el fichero de envíos");
-            } finally {
-                if (sc != null) {
-                    sc.close();
+    /**
+     * TODO: Añade los Envios al final de un fichero CSV, SIN SOBREESCRIBIR la información
+     *
+     * @param fichero Nombre del fichero CSV
+     * @return True si se realiza la operación correctamente, false en caso contrario
+     */
+    public boolean aniadirEnviosCsv (String fichero){
+        PrintWriter pw = null;
+        try {
+            pw = new PrintWriter(fichero);
+            for (int i = 0; i < envios.length; i++){
+                if (envios[i]!= null) {
+                    pw.print(envios[i].getLocalizador());
+                    pw.print(";");
+                    pw.print(envios[i].getPorte().getID());
+                    pw.print(";");
+                    pw.print(envios[i].getCliente().getEmail());
+                    pw.print(";");
+                    pw.print(envios[i].getFila());
+                    pw.print(";");
+                    pw.print(envios[i].getColumna());
+                    pw.print(";");
+                    pw.print(envios[i].getPrecio());
+                    pw.println();
+                } else {
+                    pw.println();
                 }
             }
+            return true;
+        } catch (FileNotFoundException e) {
+            return false;
+        } finally {
+            if (pw != null) {
+                pw.close();
+            }
         }
+    }
+
+    /**
+     * TODO: Lee los Envios del fichero CSV y los añade a las listas de sus respectivos Portes y Clientes
+     * @param ficheroEnvios Nombre del fichero CSV que contiene la información de los envíos
+     * @param portes        Lista de portes asociada a los envíos
+     * @param clientes      Lista de clientes asociada a los envíos
+     */
+    public static void leerEnviosCsv (String ficheroEnvios, ListaPortes portes, ListaClientes clientes) {
+        ListaEnvios listaEnvios = new ListaEnvios(clientes.getLength());
+        Scanner sc = null;
+        boolean escrito = true;
+        try {
+            sc = new Scanner(new FileReader(ficheroEnvios));
+            String linea;
+
+            while (sc.hasNextLine() && escrito) {
+                linea = sc.nextLine();
+                String[] datos = linea.split(";");
+                String localizador = datos[0];
+                Porte porte = portes.buscarPorte(datos[1]);
+                Cliente cliente = clientes.buscarClienteEmail(datos[2]);
+                int fila = Integer.parseInt(datos[3]);
+                int columna = Integer.parseInt(datos[4]);
+                double precio = Double.parseDouble(datos[5]);
+                Envio envio = new Envio(localizador, porte, cliente, fila, columna, precio);
+                if (porte != null) {
+                    porte.ocuparHueco(envio);
+                }
+
+                if (cliente != null) {
+                    cliente.aniadirEnvio(envio);
+                }
+                escrito = listaEnvios.insertarEnvio(envio);//HERE revisar el ocuparHueco
+
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("No se ha encontrado el fichero de envíos");
+        } finally {
+            if (sc != null) {
+                sc.close();
+            }
+        }
+    }
+
 }
 
 
