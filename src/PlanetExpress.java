95e6f0a5d990954e6f5af7059872e80dc1e795c0
Alejandro <alexfdzg05@gmail.com>
2023-12-22 12:09:40 +0100

    Añadida estructura en el guardarDatos(no está aún terminada)

--- src/PlanetExpress.java
+++ src/PlanetExpress.java
@@ -80,7 +80,11 @@ public class PlanetExpress {
     for (int i = 0; i < listaClientes.getLength(); i++){
         Cliente cliente = listaClientes.getCliente(i);
         if (cliente != null) {
-            listaEnvios.insertarEnvio(cliente.getEnvio(i));
+            for (int j = 0; j < cliente.getListaEnvios().getLength(); j++) {
+                if (cliente.getEnvio(j)!=null) {
+                    listaEnvios.insertarEnvio(cliente.getEnvio(i));
+                }
+            }
         }
     }
     listaEnvios.aniadirEnviosCsv(ficheroEnvios);
