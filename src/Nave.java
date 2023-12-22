570f2be5e4b814be74b966c96223805b3fe9db94
Aconi-dot <90859549+Aconi-dot@users.noreply.github.com>
2023-12-22 11:04:34 +0100

    Clase Nave completamente comentada

--- src/Nave.java
+++ src/Nave.java
@@ -1,7 +1,7 @@
 package src;
 
 /**
- * Description of the class
+ * Clase que representa una nave espacial (Nave)
  *
  * @author Paula Cabrero
  * @author Alejandro Fernández
@@ -19,12 +19,12 @@ public class Nave {
     /**
      * Constructor of the class
      *
-     * @param marca
-     * @param modelo
-     * @param matricula
-     * @param columnas
-     * @param filas
-     * @param alcance
+     * @param marca     Marca de la nave espacial
+     * @param modelo    Modelo de la nave espacial
+     * @param matricula Número de matrícula de la nave espacial
+     * @param columnas  Número de columnas en la nave espacial
+     * @param filas     Número de filas en la nave espacial
+     * @param alcance   Alcance o rango de la nave espacial
      */
     public Nave(String marca, String modelo, String matricula, int columnas, int filas, double alcance) {
         this.marca = marca;
@@ -34,21 +34,57 @@ public class Nave {
         this.filas = filas;
         this.alcance = alcance;
     }
+
+    /**
+     * Método getter para obtener la marca de la nave espacial
+     *
+     * @return La marca de la nave espacial
+     */
     public String getMarca() {
         return marca;
     }
+
+    /**
+     * Método getter para obtener el modelo de la nave espacial
+     *
+     * @return El modelo de la nave espacial
+     */
     public String getModelo() {
         return modelo;
     }
+
+    /**
+     * Método getter para obtener la matrícula de la nave espacial
+     *
+     * @return El número de matrícula de la nave espacial
+     */
     public String getMatricula() {
         return matricula;
     }
+
+    /**
+     * Método getter para obtener el número de columnas en la nave espacial
+     *
+     * @return El número de columnas en la nave espacial
+     */
     public int getColumnas() {
         return columnas;
     }
+
+    /**
+     * Método getter para obtener el número de filas en la nave espacial
+     *
+     * @return El número de filas en la nave espacial
+     */
     public int getFilas() {
         return filas;
     }
+
+    /**
+     * Método getter para obtener el alcance o rango de la nave espacial.
+     *
+     * @return El alcance o rango de la nave espacial.
+     */
     public double getAlcance() {
         return alcance;
     }
